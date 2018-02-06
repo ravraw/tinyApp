1ad9b13fa02e4b906b632037f45b6c86e789bdc5
ravindra rawat <ravinrawat.wd@gmail.com>
2018-02-06 05:38:00 +0000

    delete and edit working

--- express_server.js
+++ express_server.js
@@ -47,7 +47,6 @@ function generateRandomString() {
   randomId += String.fromCharCode(Math.floor(Math.random() * (122 - 97) ) + 97);
   }
   randomId.toString();
-  console.log(randomId);
   return randomId ;
 }
 
@@ -88,12 +87,9 @@ app.post("/register", (req, res) => {
   if(req.body.email === '' || req.body.password === '' ){
     res.status(400).send("Error: Enter a valid Email and Password")
   }
-  console.log(`${req.body.email}`)
 
   for(key in users){
     if (users[key].email === `${req.body.email}`){
-        //console.log( working);
-        // not working
       return res.status(400).send("Error: Email id already taken");
     }
   }
@@ -108,7 +104,6 @@ app.post("/register", (req, res) => {
 
       res.cookie("user_id",`${randomId}`);
       res.render("urls_register");
-      console.log(users);
 });
 
 // -------LOGIN PAGES--------------///
@@ -119,8 +114,6 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-
-  console.log(req.body);
   for(let key in users){
     if (users[key].email === req.body.email && users[key].password === req.body.password){
         users[key].loggedin = true ;
@@ -136,9 +129,8 @@ app.post("/login", (req, res) => {
 // ----------- LOGOUT -----------------//
 
 app.post("/logout",(req, res) => {
-   console.log(req);
+
   req.session.user_id = null;
-  console.log(req);
   res.redirect('/');
 });
 
@@ -166,7 +158,7 @@ app.get("/hello", (req, res) => {
 
 //------------LINK TO NEW --------------//
 
-// app.get('/urls/new', (req, res) => {
+// app.post('/urls/new', (req, res) => {
 
 //     let templateVars = {
 //       users: users,
@@ -174,14 +166,13 @@ app.get("/hello", (req, res) => {
 //       user_id:req.cookies.user_id,
 //       }
 
-//    res.render("urls_new",templateVars);
+//    res.render("/urls");
 
 // });
 
 
 
 app.get("/urls/new", (req, res) => {
-  //console.log(req);
   if(req.cookies.user_id === undefined){
     return res.status(401).send('Error: 401: Not autorized , Please <a href="/register"> Register </a>');
   }
@@ -206,60 +197,37 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls", (req, res) => {
    let templateVars = {
-     urls: urlDatabase,
-    // users: users,
-    // urls: urlDatabase,
-    // longURL: urlDatabase.url.longURL,
-    // user_id:req.cookies.user_id,
+    urls: urlDatabase,
+    users: users,
+    urls: urlDatabase,
+    user_id:req.cookies.user_id,
   };
-  console.log(urlDatabase.b2xVn2["longURL"]);
-  console.log(users.userRandomID.id);
   res.render("urls_index",templateVars);
 });
 
+
 app.post("/urls", (req, res) => {
-  if (userChecker(req.session.user_id)) {
-    let newID = generateRandomNumber();
-    urlDatabase[newID] = {
-      longURL: req.body.longURL,
-      userID: req.session.user_id
-    };
-    res.redirect('/urls');
-  } else {
-    res.status(401).send('Error: 401: You are not authorized, Please <a href="/"> Login </a>');
-  }
+  //if (checkUserStatus(req.cookies.user_id.id)) {
+    let newID = generateRandomString();
+     urlDatabase[newID] = {
+      longURL: req.body["longURL"],
+      userID: req.cookies.user_id.id
+     }
+  res.redirect(req.body["longURL"]);
 });
-
-// app.post("/urls", (req, res) => {
-//   if (checkUserStatus(req.cookies.user_id.id)) {
-//     let newID = generateRandomString();
-//      urlDatabase[newID] = {
-//       longURL: req.body.longURL,
-//       userID: req.cookies.user_id.id
-//     };
-
-//   res.redirect(`/u/${newID}`);
-//   console.log(urlDatabase);
-// }
-// });
 
 
 //-----------------
 
 app.get("/u/:shortURL", (req, res) => {
-  console.log(req.params);
   var shortURL = req.params.shortURL;
-  console.log(shortURL);
   var longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
-  //res.status(301).send({ error: 'temporary redirect"'});
 });
 
 // -------------------------------
 
 app.get("/urls/:id", (req, res) => {
-  //console.log('/urls/:id');
- console.log(req);
   let templateVars = {
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id],
@@ -270,31 +238,20 @@ app.get("/urls/:id", (req, res) => {
 
 });
 
-
-
-
-//---------------------------------
-app.post("/urls/:id/delete", (req, res) => {
-  if (userChecker(req.session.user_id)) {
-    delete urlDatabase[req.params.id];
-    res.redirect('/urls');
-  } else {
-    res.status(403).send('403: You are not allowed to delete this');
-  }
-});
-
-
 //-------------------DELETE ------------------//
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   if(checkUserStatus(req.cookies.user_id.id)){
-    console.log(req.params);
+     //console.log(req.cookies.user_id);
+    //console.log(req.cookies.user_id.id);
+   // console.log(req.params);
     var shortURL = req.params.shortURL;
+    if(req.cookies.user_id['id'] === urlDatabase[shortURL]['userID']){
     delete urlDatabase[req.params.shortURL];
+    }
     res.redirect("/urls");
-  }else {
-    res.status(403).send('403: Not authorised to delete ');
   }
+    res.status(403).send('403: Not authorised to delete ');
 
 });
 
@@ -302,29 +259,32 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 app.get("/urls/:shortURL/edit",(req,res)=>{
    if(checkUserStatus(req.cookies.user_id.id)){
-    console.log(req);
-      let templateVars = {
+
+    let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.id],
     users: users,
   };
+  shortURL = req.params.shortURL;
+  //console.log(req);
+  if(req.cookies.user_id['id'] === urlDatabase[shortURL]['userID']){
 
-  res.render("urls_show", templateVars);
-
+      res.render("urls_show", templateVars);
+    }
 }else {
     res.status(403).send('403: Not authorised to edit ');
   }
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
-  if(checkUserStatus(req.cookies.user_id.id)){
-    urlDatabase[req.params.shortURL] = req.body.updatedURL;
-    res.redirect("/urls");
-  }else {
-    res.status(403).send('403: Not authorised to edit ');
-  }
-
-
+  console.log(req);
+    if(checkUserStatus(req.cookies.user_id.id)){
+     if(req.cookies.user_id['id'] === urlDatabase[shortURL]['userID']){
+  urlDatabase[req.params.shortURL].longURL = req.body.updatedURL;
+      }
+  res.redirect("/urls");
+}
+   //res.status(403).send('403: Not authorised to edit ');
 });
 
 
