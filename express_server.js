75b480d174177f98df314766ab071b6edcafcb97
ravindra rawat <ravinrawat.wd@gmail.com>
2018-02-07 03:46:29 +0000

    required links completed

--- express_server.js
+++ express_server.js
@@ -1,297 +1,285 @@
-var express = require('express');
-var cookieParser = require('cookie-parser')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-var app = express()
-app.use(cookieParser())
+app.set('view engine', 'ejs');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Encyrpts cookies
+const bcrypt = require('bcrypt');
+
+// Handles cookies
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  secret: 'SuperSecureSecret',
+}));
 
 
-app.set("view engine", "ejs");
+//----------URL DATABASE ----------------//
 
-var PORT = process.env.PORT || 8080; // default port 8080
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-
-var urlDatabase = {
-  "b2xVn2":{
-      longURL:"http://www.lighthouselabs.ca",
-      userID:"a4a536"
+const urlDatabase = {
+  b2xVn2: {
+    longURL: 'http://www.lighthouselabs.ca',
+    userID: 'a4a536',
   },
-  "9sm5xK":{
-      longURL:"http://www.google.com",
-      userID:"agdth7"
+  '9sm5xK': {
+    longURL: 'http://www.google.com',
+    userID: 'agdth7',
   },
-}
-
-
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-}
-
-
-
-function generateRandomString() {
-  var randomId = '' ;
- //var  randomId = Math.random().toString(36).substring(2,5) + Math.random().toString(36).substring(2,5);;
- while(randomId.length < 6){
-  randomId += Math.floor(Math.random()*9);
-  randomId += String.fromCharCode(Math.floor(Math.random() * (122 - 97) ) + 97);
-  }
-  randomId.toString();
-  return randomId ;
-}
-
-//--------------CHECK IF USER IS REGISTERED OR LOGGED IN -----//
-const checkUserRegistration = (email, password) => {
-  if (password === undefined) {
-    for (user in users) {
-      if (users[user].email === email){
-        return true;
-      }
-    } return false;
-  }
 };
 
-const checkUserStatus = (currentUser) => {
+// -------------USERS DATABASE --------------//
+const users = {
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: bcrypt.hashSync('purple-monkey-dinosaur', 10),
+  },
+  user2RandomID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: bcrypt.hashSync('dishwasher-funk', 10),
+  },
+};
 
-  for (let user in users) {
+// ------------FUNCTION GENERATES  RANDOM ID-----------//
+
+function generateRandomString() {
+  let randomId = '';
+  while (randomId.length < 6) {
+    randomId += Math.floor(Math.random() * 9);
+    randomId += String.fromCharCode(Math.floor(Math.random() * (122 - 97)) + 97);
+  }
+  randomId.toString();
+  return randomId;
+}
+
+//-------------FUNCTION CHECKS USER STATUS -------------//
+
+const checkUserStatus = (currentUser) => {
+  for (const user in users) {
     if (user === currentUser) {
       return true;
     }
   } return false;
-
 };
 
-
-
-//-------------- REGISTER PAGE -------------//
-
-
-app.get("/register", (req, res) => {
-
-  res.render("urls_register");
-});
-
-
-app.post("/register", (req, res) => {
-  let randomId = generateRandomString();
-  if(req.body.email === '' || req.body.password === '' ){
-    res.status(400).send("Error: Enter a valid Email and Password")
-  }
-
-  for(key in users){
-    if (users[key].email === `${req.body.email}`){
-      return res.status(400).send("Error: Email id already taken");
+//-------------FUNCTION IDENTIFYES USERS OWN URLS ---------//
+function urlsForUser(id) {
+  let usersUrls = {};
+  for (url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      usersUrls[url] = urlDatabase[url];
     }
   }
-      //res.cookie("user_id",`${randomId}`);
-      users[randomId]={
-        id: `${randomId}`,
-        email: `${req.body.email}`,
-        password: `${req.body.password}`,
-        loggedIn: false,
+  return usersUrls;
+}
 
-    }
-
-      res.cookie("user_id",`${randomId}`);
-      res.render("urls_register");
-});
-
-// -------LOGIN PAGES--------------///
-
-app.get("/login", (req, res) => {
-
-  res.render("login");
-});
-
-app.post("/login", (req, res) => {
-  for(let key in users){
-    if (users[key].email === req.body.email && users[key].password === req.body.password){
-        users[key].loggedin = true ;
-        res.cookie("user_id",users[key]);
-        return res.redirect("/");
-    }
-  }
-  return res.status(403).send("Error: Please enter a registered Email or Password");
-});
+// ----------------- ALL GET REQUESTS ---------------------------//
 
 
-//-------------------------------------//
-// ----------- LOGOUT -----------------//
 
-app.post("/logout",(req, res) => {
-
-  req.session.user_id = null;
-  res.redirect('/');
-});
-
-// -------------------------------------//
-
-//---------------ROOT ---------------//
-
-app.get('/', (req,res)=>{
-  if(checkUserStatus)
-  res.end('hello!!');
-
-})
-
-//--------------JSON -------------//
-app.get("/urls.json", (req, res) => {
+app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-//------------------HELL0------------//
+//-----Loads the root or login - register page---//
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/', (req, res) => {
+   res.render('urls_register.ejs');
+});
+
+//-----Directs to register page ------//
+
+app.get('/register', (req, res) => {
+  res.render('urls_register');
 });
 
 
-//------------LINK TO NEW --------------//
+//--------directs to login Page ------//
+app.get('/login', (req, res) => {
+  const templateVars = {
+    users,
+    urls: urlDatabase,
+    user_id: req.session.user_id,
+  };
+  res.render('login', templateVars);
+});
 
-// app.post('/urls/new', (req, res) => {
+// ------Checksif user is logged-in before redering create a new tiny url page------//
 
-//     let templateVars = {
-//       users: users,
-//       urls: urlDatabase,
-//       user_id:req.cookies.user_id,
-//       }
-
-//    res.render("/urls");
-
-// });
-
-
-
-app.get("/urls/new", (req, res) => {
-  if(req.cookies.user_id === undefined){
+app.get('/urls/new', (req, res) => {
+  if (req.session.user_id === undefined) {
     return res.status(401).send('Error: 401: Not autorized , Please <a href="/register"> Register </a>');
   }
-    if(checkUserStatus(req.cookies.user_id.id)) {
-      let templateVars = {
-      users: users,
+  if (checkUserStatus(req.session.user_id)) {
+    const templateVars = {
+      users,
       urls: urlDatabase,
-      user_id:req.cookies.user_id,
-      }
-    return res.render("urls_new",templateVars);
+      user_id: req.session.user_id,
+    };
+    return res.render('urls_new', templateVars);
+  }
 
-    }
-
-      return res.status(401).send('Error: 401: Not autorized , Please <a href="/login"> Login </a>');
-
+  return res.status(401).send('Error: 401: Not autorized , Please <a href="/login"> Login </a>');
 });
 
+// -----Renders the URLs ofthe logged in user else send to login page ------//
 
-
-
-// -------------------URLS --------------//
-
-app.get("/urls", (req, res) => {
-   let templateVars = {
-    urls: urlDatabase,
-    users: users,
-    urls: urlDatabase,
-    user_id:req.cookies.user_id,
-  };
-  res.render("urls_index",templateVars);
+app.get('/urls', (req, res) => {
+  if (checkUserStatus(req.session.user_id)) {
+    const templateVars = {
+      urls: urlsForUser(req.session.user_id),
+      users,
+      user_id: req.session.user_id,
+    };
+    res.render('urls_index', templateVars);
+  } else {
+    res.redirect('/login');
+  }
 });
 
+//------Link to show the web page of short urls ------//
 
-app.post("/urls", (req, res) => {
-  //if (checkUserStatus(req.cookies.user_id.id)) {
-    let newID = generateRandomString();
-     urlDatabase[newID] = {
-      longURL: req.body["longURL"],
-      userID: req.cookies.user_id.id
-     }
-  res.redirect(req.body["longURL"]);
-});
-
-
-//-----------------
-
-app.get("/u/:shortURL", (req, res) => {
-  var shortURL = req.params.shortURL;
-  var longURL = urlDatabase[shortURL].longURL;
+app.get('/u/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
-// -------------------------------
+// ----- shows the edit page if the user is owner of the short url -----//
+app.get('/urls/:shortURL/edit', (req, res) => {
+  if (checkUserStatus(req.session.user_id)) {
+    const templateVars = {
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.id],
+      users,
+    };
+    shortURL = req.params.shortURL;
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id],
-    users: users,
-  };
-
-  res.render("urls_show", templateVars);
-
-});
-
-//-------------------DELETE ------------------//
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  if(checkUserStatus(req.cookies.user_id.id)){
-     //console.log(req.cookies.user_id);
-    //console.log(req.cookies.user_id.id);
-   // console.log(req.params);
-    var shortURL = req.params.shortURL;
-    if(req.cookies.user_id['id'] === urlDatabase[shortURL]['userID']){
-    delete urlDatabase[req.params.shortURL];
+    if (req.session.user_id === urlDatabase[shortURL].userID) {
+      res.render('urls_show', templateVars);
     }
-    res.redirect("/urls");
-  }
-    res.status(403).send('403: Not authorised to delete ');
-
-});
-
-//--------------------EDIT -------------------//
-
-app.get("/urls/:shortURL/edit",(req,res)=>{
-   if(checkUserStatus(req.cookies.user_id.id)){
-
-    let templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.id],
-    users: users,
-  };
-  shortURL = req.params.shortURL;
-  //console.log(req);
-  if(req.cookies.user_id['id'] === urlDatabase[shortURL]['userID']){
-
-      res.render("urls_show", templateVars);
-    }
-}else {
+  } else {
     res.status(403).send('403: Not authorised to edit ');
   }
 });
 
-app.post("/urls/:shortURL/edit", (req, res) => {
-  console.log(req);
-    if(checkUserStatus(req.cookies.user_id.id)){
-     if(req.cookies.user_id['id'] === urlDatabase[shortURL]['userID']){
-  urlDatabase[req.params.shortURL].longURL = req.body.updatedURL;
-      }
-  res.redirect("/urls");
-}
-   //res.status(403).send('403: Not authorised to edit ');
+//----------------------------------------------------------//
+
+// -----------------POST REQUESTS ---------------------------//
+
+
+
+// ---- registers a new user and checks the inputs are correct ----//
+app.post('/register', (req, res) => {
+  const randomId = generateRandomString();
+  if (req.body.email === '' || req.body.password === '') {
+    res.status(400).send('Error: Enter a valid Email and Password');
+  }
+  for (key in users) {
+    if (users[key].email === `${req.body.email}`) {
+      return res.status(400).send('Error: Email id already taken');
+    }
+  }
+  users[randomId] = {
+    id: `${randomId}`,
+    email: `${req.body.email}`,
+    password: bcrypt.hashSync(req.body.password, 10),
+    loggedIn: false,
+
+  };
+  req.session.user_id = `${randomId}`;
+  res.redirect('/login');
 });
 
 
+// ----------Checks the inputs for the user trying to login and show the urls of the owner -----//
+app.post('/login', (req, res) => {
+  let loggedin = false;
+
+  for (const key in users) {
+    if (users[key].email === req.body.email && bcrypt.compareSync(req.body.password, users[key].password)) {
+      loggedin = true;
+    }
+  }
+  if (loggedin) {
+    const templateVars = {
+    urls: urlsForUser(req.session.user_id),
+    users,
+    user_id: req.session.user_id,
+  };
+    res.render('urls_index', templateVars);
+  } else {
+    res.redirect('/register');
+  }
+
+});
+
+// ------Logouts the user and end the session ----------//
+
+app.post('/logout', (req, res) => {
+  console.log('1');
+  console.log(req);
+  req.session = null;
+  res.redirect('/');
+});
+//-------Adds a new tiny url to the owners list -------//
+app.post('/urls/new', (req, res) => {
+  res.redirect('/urls');
+});
+
+app.post('/urls', (req, res) => {
+  const newID = generateRandomString();
+  urlDatabase[newID] = {
+    longURL: req.body.longURL,
+    userID: req.session.user_id,
+  };
+  const templateVars = {
+    urls: urlsForUser(req.session.user_id),
+    users,
+    user_id: req.session.user_id,
+  };
+  res.render('urls_index', templateVars);
+});
+
+
+// ---------deletes the short url if belongs to the current user -----//
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  if (checkUserStatus(req.session.user_id)) {
+
+    if (req.session.user_id === urlDatabase[req.params.shortURL].userID) {
+      delete urlDatabase[req.params.shortURL];
+    }
+    res.redirect('/urls');
+    return;
+  }
+  res.status(403).send('403: Not authorised to delete ');
+});
+
+
+// --------edits the short url if belongs to the current user -------//
+
+app.post('/urls/:shortURL/edit', (req, res) => {
+
+  if (checkUserStatus(req.session.user_id)) {
+    if (req.session.user_id === urlDatabase[shortURL].userID) {
+      urlDatabase[req.params.shortURL].longURL = req.body.updatedURL;
+    }
+    res.redirect('/urls');
+  } else {
+    res.status(403).send('403: Not authorised to edit ');
+  }
+});
+
+//----------------------------------------------------------//
+
 //----------------------------------------------
-app.listen(PORT,()=>{
-
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-
 });
 
